a838fdc1f7462257df0b22c4518bd2292a011065
windscroll <2321864583@qq.com>
2026-05-27 00:01:02 +0800

    Delete scr/main directory

--- scr/main/java/com/code/mapper/SysUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.code.mapper;
-
-import com.code.entity.SysUser;
-import org.apache.ibatis.annotations.Param;
-
-public interface SysUserMapper {
-    SysUser login(@Param("username") String username,@Param("password") String password);
-}
